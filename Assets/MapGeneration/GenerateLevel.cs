36cbb40ee9a0b9db8a0f7edb2e0f1ed1ca797c0b
Okko Ahonen <okko.a.ahonen@gmail.com>
2024-11-15 15:33:09 +0200

    RoomGeneration v1

--- Assets/MapGeneration/GenerateLevel.cs
+++ Assets/MapGeneration/GenerateLevel.cs
@@ -12,6 +12,52 @@ public class GenerateLevel : MonoBehaviour
     public Sprite unexploretRoom;
     public Sprite TreasureRoom;
 
+    void DrawRoomOnMap(Sprite s, Vector2 Location)
+    {
+        GameObject Maptile = new GameObject("MapTile");
+        Image RoomImage = Maptile.AddComponent<Image>();
+        RoomImage.sprite = s;
+        RectTransform rectTransform = RoomImage.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(Levels.height, Levels.height) * Levels.IconScale;
+        rectTransform.position = Location * (Levels.IconScale * Levels.height * Levels.scale + (Levels.padding * Levels.height * Levels.scale));
+        RoomImage.transform.SetParent(transform, false);
+    }
+
+    void Generate(Vector2 Location)
+    {
+        if (Random.value > Levels.RoomGenerationChance)
+        {
+            
+            DrawRoomOnMap(Levels.unexploredroomIcon, Location + new Vector2(0, 1));
+            Generate(Location + new Vector2(0, 1));
+        }
+
+        //left
+        if (Random.value > Levels.RoomGenerationChance)
+        {
+            
+            DrawRoomOnMap(Levels.unexploredroomIcon, Location +  new Vector2(1, 0));
+            Generate(Location + new Vector2(1, 0));
+        }
+
+        //right
+        if (Random.value > Levels.RoomGenerationChance)
+        {
+            
+            DrawRoomOnMap(Levels.unexploredroomIcon, Location + new Vector2(-1, 0));
+            Generate(Location + new Vector2(-1, 0));
+        }
+
+        //Down
+        if (Random.value > Levels.RoomGenerationChance)
+        {
+            
+            DrawRoomOnMap(Levels.unexploredroomIcon, Location + new Vector2(0, -1));
+            Generate(Location + new Vector2(0, -1));
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,18 +67,39 @@ public class GenerateLevel : MonoBehaviour
         Levels.currentroomIcon = CurrentRoom;       //4
         Levels.unexploredroomIcon = unexploretRoom; //5
 
+        DrawRoomOnMap(Levels.currentroomIcon, new Vector2(0, 0)); //Star room draw
 
 
+        //up
+        if (Random.value > Levels.RoomGenerationChance)
+        {
+            Generate(new Vector2(0, 1));
+            DrawRoomOnMap(Levels.unexploredroomIcon, new Vector2(0, 1));
+        }
 
+        //left
+        if (Random.value > Levels.RoomGenerationChance) 
+        {
+            Generate(new Vector2(-1, 0));
+            DrawRoomOnMap(Levels.unexploredroomIcon, new Vector2(1, 0)); 
+        }
 
+        //right
+        if (Random.value > Levels.RoomGenerationChance)
+        {
+            Generate(new Vector2(1, 0));
+            DrawRoomOnMap(Levels.unexploredroomIcon, new Vector2(-1, 0));
+        }
 
-        GameObject Maptile = new GameObject("MapTile");
-        Image RoomImage = Maptile.AddComponent<Image>();
-        RoomImage.sprite = Levels.DefaultRoomIcon;
-        RectTransform rectTransform = RoomImage.GetComponent<RectTransform>();
-        rectTransform.sizeDelta = new Vector2(Levels.height, Levels.height) * Levels.IconScale;
-        RoomImage.transform.SetParent(transform, false);    
+        //Down
+        if (Random.value > Levels.RoomGenerationChance)
+        {
+            Generate(new Vector2(1, -1));
+            DrawRoomOnMap(Levels.unexploredroomIcon, new Vector2(0, -1));
+        }
     }
+
+
 
     // Update is called once per fram
 }
--- Assets/MapGeneration/Levels.cs
+++ Assets/MapGeneration/Levels.cs
@@ -9,6 +9,9 @@ public static class Levels
 
     public static float scale = 1.0f;
     public static float IconScale = 0.1f;
+    public static float padding = 0.01f;
+
+    public static float RoomGenerationChance = 0.5f;
 
     public static Sprite TreasureRoomIcon;
     public static Sprite BossRoomIcon;
