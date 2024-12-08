603161f6e549772d93debb9b978a2e2942ee69c7
Crypticbearcub <129151537+Crypticbearcub@users.noreply.github.com>
2024-12-08 20:03:47 +0200

    Combat & enemy statit

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    [SerializeField] int hitDamage = 3;
+    public int hitDamage = 3;
+    public bool hit = false;
+    public float currentHealth;
+    public float enemyHealth;
+
+    public GameObject player;
+    public GameObject sword;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject player = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void enemyDamage(int hit)
+    {
     }
 }
