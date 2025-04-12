54d8ee43cfc8c9a7c61bef32f8fce8225fc54124
Okko Ahonen <okko.a.ahonen@gmail.com>
2025-04-12 15:51:53 +0300

    Animaatiot toimii

--- Assets/HouseAnimationManager.cs
+++ Assets/HouseAnimationManager.cs
@@ -5,24 +5,43 @@ using UnityEngine;
 public class HouseAnimationManager : MonoBehaviour
 {
     public Animator Roof;
-    // Start is called before the first frame update
+    public Animator Door;
+
+    public bool hasExitedTheHouse;
+
     void Start()
     {
         Roof = GameObject.Find("CabinTop").GetComponent<Animator>();
-        
+        Door = GameObject.Find("CabinDoor").GetComponent<Animator>();
+        if (Roof == null)
+        {
+            Debug.LogError("Animator-komponenttia ei löydy CabinTop-objektista!");
+        }
+        if (Door == null)
+        {
+            Debug.LogError("Animator-komponenttia ei löydy CabinDoor-objektista!");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Roof != null)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.GetKeyDown(KeyCode.Space)) 
-            { 
-                Roof.SetBool("InHouse", true);
-                Debug.Log("InHouse set to true");
-            }
+            // Hae nykyinen "InHouse"-arvo (voidaan hakea vain yhdestä Animatorista, koska niiden pitäisi olla synkronissa)
+            bool currentState = Roof != null ? Roof.GetBool("InHouse") : false;
+            bool newState = !currentState;
 
+            // Aseta arvo molemmille Animatorille
+            if (Roof != null)
+            {
+                Roof.SetBool("InHouse", newState);
+                Debug.Log("Roof InHouse asetettu arvoon " + newState);
+            }
+            if (Door != null)
+            {
+                Door.SetBool("InHouse", newState);
+                Debug.Log("Door InHouse asetettu arvoon " + newState);
+            }
         }
     }
 }
