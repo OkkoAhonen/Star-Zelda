033afe4c15916eb0964cfeac13d40585de6b537f
Okko Ahonen <okko.a.ahonen@gmail.com>
2025-04-12 14:22:02 +0300

    hommat sisään

--- Assets/ShopManager.cs
+++ Assets/ShopManager.cs
@@ -1,38 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
     public Item[] shopItems;
     public int shopCount = 1;
-    public GameObject shopPrefab;
 
     [HideInInspector] public GameObject inventoryManagerGameObject;
     public MoneyManager moneyManager;
     public InventoryManager inventoryManager;
     public ItemShop1Script itemShop1Script;
 
+    public GameObject shop; //Aseta Inspectorissa
+    public GameObject ExitButton;//Aseta Inspectorissa
+
     public ShopItemDisplay[] shopDisplays;
-    public GameObject shopGameObject; //Aseta inspectorissa
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        
         inventoryManagerGameObject = GameObject.FindGameObjectWithTag("InventoryManager");
         moneyManager = inventoryManagerGameObject.GetComponent<MoneyManager>();
         inventoryManager = inventoryManagerGameObject.GetComponent<InventoryManager>();
 
         shopCount = shopItems.Length;
-        Debug.Log("Laitettu2");
+        Debug.Log("ShopManager alustettu");
+    }
+
+    // Uusi metodi itemien alustamiseen
+    public void InitializeShopItems()
+    {
         for (int i = 0; i < shopItems.Length && i < shopDisplays.Length; i++)
         {
-            shopDisplays[i].SetItem(shopItems[i]);
+            if (shopDisplays[i] != null)
+            {
+                shopDisplays[i].SetItem(shopItems[i]);
+            }
+            else
+            {
+                Debug.LogWarning($"shopDisplays[{i}] on null!");
+            }
         }
     }
-    //BuyItem funktiot aktivoituvat kun painaa buttonia
+
+    public void OpenShop()
+    {
+        shop.SetActive(true);
+        ExitButton.SetActive(true);
+        InitializeShopItems(); // Alustaa itemit, kun kauppa avataan
+    }
+
+    public void CloseShop()
+    {
+        shop.SetActive(false); // Korjattu: sulkee kaupan
+        ExitButton.SetActive(false);
+    }
+
     public void BuyItem(int index)
     {
         if (index >= 0 && index < shopItems.Length)
@@ -42,17 +63,4 @@ public class ShopManager : MonoBehaviour
             inventoryManager.AddItem(item);
         }
     }
-
-    public void OpenShop()
-    {
-        GameObject shop = GameObject.FindWithTag("Shop");
-        shopGameObject.SetActive(true);
-    }
-    public void CloseShop()
-    {
-        GameObject shop = GameObject.FindWithTag("Shop");
-        shop.SetActive(true);
-    }
-
-
 }
